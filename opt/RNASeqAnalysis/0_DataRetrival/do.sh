31998eb7bd330c507b66c0795b445e0cb3c4921c
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2021-01-31 19:44:50 +0800

    RNA Download Coding Finished

--- opt/RNASeqAnalysis/0_DataRetrival/do.sh
+++ opt/RNASeqAnalysis/0_DataRetrival/do.sh
@@ -5,6 +5,8 @@ DN="$(readlink -f "$(dirname "${0}")")"
 cd "${DN}"
 . ../etc/path.sh
 . ../lib/libexec.sh
+. ../etc/head.sh
+. ../lib/head.sh
 function my_rename() {
 	printf ".fastq -> .fq"
 	if ls *.fastq* &>>/dev/null; then
@@ -16,7 +18,7 @@ function my_rename() {
 	printf ".fq -> .fq.gz"
 	if ls *.fq &>>/dev/null; then
 		for fn in *.fq; do
-			cat ../lib/head.sh exec/gzip.sh |\
+			cat ../etc/head.sh ../lib/head.sh exec/gzip.sh |\
 			sed "s;TARGET=TARGET;TARGET=$(echo ${fn});" |\
 			sed "s;WD=WD;WD=$(pwd);" |\
 			${bsub}
@@ -26,7 +28,7 @@ function my_rename() {
 	if ls *.fq.gz &>>/dev/null; then
 		for fn in *.fq.gz; do
 			echo "Initializing fastqc for ${fn}"
-			cat ../lib/head.sh exec/fastqc.sh |\
+			cat ../etc/head.sh ../lib/head.sh exec/fastqc.sh |\
 			sed "s;TARGET=TARGET;TARGET=$(echo ${fn});" |\
 			sed "s;WD=WD;WD=$(pwd);" |\
 			${bsub}
@@ -39,23 +41,23 @@ function tail_name() {
 }
 base_url='https://www.ebi.ac.uk/ena/portal/api/search?result=read_run&dataPortal=ENA&fields=fastq_ftp,fastq_aspera,fastq_md5&limit=0&query=run_accession='
 if [ ! -f accession.tsv ];then
-cat ../sample.conf | grep -v '^$' | grep -v '^\#' | while read accession;do
+cat ../sample.conf | grep -v '^$' | grep -v '^\#' | cut -f 2 -d ' ' | while read accession;do
 	echo "Parsing ${accession}"
 	printf '\n#' >> accession.tsv
 	wget -O tmpt "${base_url}${accession}"
-	rm tmpt
 	cat tmpt>> accession.tsv
+	rm tmpt
 done
 fi
 
 if ${myascp}; then
 	echo "Will use ascp to accelerate download."
 	cat accession.tsv | grep -v '^$' | grep -v '^\#' | cut -f 3 | tr ';' '\n' | while read line; do
-		! ls "$(tail_name "${line}")".* &>/dev/null && ascp -QT -l 300m -P33001 -i asperaweb_id_dsa.openssh era-fasp@"${line}"
+		! ls "$(tail_name "${line}")".* &>/dev/null && ascp -QT -l 300m -P33001 --overwrite=diff+older -i "${ascp_etc}"/asperaweb_id_dsa.openssh era-fasp@"${line}" .
 	done
 	my_rename
 fi
-for prog in aria2c axel wget curl; do
+for prog in aria2c axel wget; do
 	if eval "\${my${prog}}"; then
 		echo "Will use ${prog} to accelerate download."
 		cat accession.tsv | grep -v '^$' | grep -v '^\#' | cut -f 2 | tr ';' '\n' | while read line; do
@@ -63,7 +65,6 @@ for prog in aria2c axel wget curl; do
 			! ls "$(tail_name "${line}")".* &>/dev/null && "${prog}" "ftp://${line}"
 			echo "  [done]"
 		done
-
 		my_rename
 	fi
 done
--- opt/RNASeqAnalysis/lib/head.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-. "${HOME}"/LinuxMiniPrograms/lib/libdo
-GENOME_FASTA=GENOME_FASTA
-GENOME_FASTA_INDEX=GENOME_FASTA_INDEX
-TARGET=TARGET
-WD=WD
-LIBDO_LOG_MODE=4
-LIBDO_LOG="${WD}/${TARGET}_$(date +%Y-%m-%d_%H-%M-%S).log"
-
