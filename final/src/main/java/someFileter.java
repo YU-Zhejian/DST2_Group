aedb0d6cb3f7b09438b6ff2eb47a49ddbd3ff9f7
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2021-04-18 15:39:12 +0800

    IDEA support added

--- final/src/main/java/someFileter.java
+++ final/src/main/java/someFileter.java
@@ -1,31 +1,29 @@
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
 
 @WebFilter("/hello")
 public class someFileter implements Filter {
 
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-			throws IOException, ServletException {
-		
-//		if(request.getSession())
-		HttpServletRequest hreq = (HttpServletRequest)request; 
-		HttpServletResponse hres = (HttpServletResponse)response; 
-        if(hreq.getSession().getAttribute("username")!=null) {
-    		chain.doFilter(request, response);
-        }else {
-        	hres.sendRedirect(hreq.getContextPath()+"/login.jsp");
-        }
-		
-		
-		
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        //		if(request.getSession())
+        HttpServletRequest hreq = (HttpServletRequest) request;
+        HttpServletResponse hres = (HttpServletResponse) response;
+        if (hreq.getSession().getAttribute("username") != null) {
+            chain.doFilter(request, response);
+        } else {
+            hres.sendRedirect(hreq.getContextPath() + "/login.jsp");
+        }
+    }
+
+    @Override
+    public void destroy() {}
 }
