50c9817840c81c2d03c20b90ec29378834a78037
Yaqi-SU <61215323+Yaqi-SU@users.noreply.github.com>
2021-04-22 09:13:42 +0800

    Update Dst2Application.java

--- src/main/java/com/example/Dst2Application.java
+++ src/main/java/com/example/Dst2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling //To eable the task be excecuted as scheduled (fixDelay=1 week)
 public class Dst2Application {
 
     public static void main(String[] args) {
