b69c691a5af05c61cfa72f97369967bd17ffc82c
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2021-04-23 19:41:21 +0800

    Primary key notation added to user and sample

--- src/main/java/com/example/bean/User.java
+++ src/main/java/com/example/bean/User.java
@@ -1,51 +1,53 @@
 package com.example.bean;
 
-import java.util.Date;
+import javax.persistence.Id;
 
 /**
  * Annotated Variant Calling Format (VCF) uploaded by the users
+ *
  * @author Zhejian YU
  */
 public class User {
-  private int id;
-  private String userName;
-  private String passwd;
+    @Id private int id;
+    private String userName;
+    private String passwd;
 
-  public User() {}
+    public User() {}
 
-  /**
-   * Constructor for Annotated Variant Calling Format (VCF) uploaded by the users
-   * @param id Index inside database
-   * @param userName Date it was uploaded
-   * @param passwd By whom it was uploaded
-   */
-  public User(int id,  String userName, String passwd) {
-    this.id = id;
-    this.userName = userName;
-    this.passwd = passwd;
-  }
+    /**
+     * Constructor for Annotated Variant Calling Format (VCF) uploaded by the users
+     *
+     * @param id Index inside database
+     * @param userName Date it was uploaded
+     * @param passwd By whom it was uploaded
+     */
+    public User(int id, String userName, String passwd) {
+        this.id = id;
+        this.userName = userName;
+        this.passwd = passwd;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public String getPasswd() {
-    return passwd;
-  }
+    public String getPasswd() {
+        return passwd;
+    }
 
-  public void setPasswd(String passwd) {
-    this.passwd = passwd;
-  }
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
 
-  public String getUserName() {
-    return userName;
-  }
+    public String getUserName() {
+        return userName;
+    }
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
